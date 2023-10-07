c942541f8934b0efb111c157ec122762cb6fc0b4
haeji1124 <46364329+haeji1124@users.noreply.github.com>
2023-10-08 02:48:17 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,36 +3,18 @@ from bs4 import BeautifulSoup
 import csv
 import json
 
-response = requests.get("http://paullab.synology.me/stock.html")
+response = requests.get("https://ridibooks.com/category/new-releases/2200")
 
 response.encoding = 'utf-8'
 html = response.text
 
 soup = BeautifulSoup(html, 'html.parser')
 
-oneStep = soup.select('.main')[2]
-twoStep = oneStep.select('tbody > tr')[1:]
-
-날짜 = []
-종가 = []
-전일비 = []
-거래량 = []
-
-for i in twoStep:
-    날짜.append(i.select('td')[0].text)
-    종가.append(int(i.select('td')[1].text.replace(',', '')))
-    전일비.append(int(i.select('td')[2].text.replace(',', '')))
-    거래량.append(int(i.select('td')[6].text.replace(',', '')))
-
+bookservices = soup.select('.fig-rs5q24')
 l = []
+for no, book in enumerate(bookservices, 1):
+    l.append(f"{no} {book.text.strip()}")
 
-for i in range(len(날짜)):
-    l.append({
-        '날짜':날짜[i],
-        '종가':종가[i],
-        '전일비':전일비[i],
-        '거래량':거래량[i],
-        })
 
 #파일을 한 번 쓴다.
 with open('data.js', "w", encoding="UTF-8-sig") as f_write:
